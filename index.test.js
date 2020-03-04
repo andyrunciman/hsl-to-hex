1e4721424198a1c54d8a13df6a226f9e32bcfb20
Andy Runciman <andyrunciman@andys-mbp.home>
2020-03-04 20:40:34 +0000

    fixed testing

--- index.test.js
+++ index.test.js
@@ -1,6 +1,10 @@
-const assert = require('assert')
-describe('it should', () => {
-  it('should', () => {
-    assert.ok(true)
-  })
-})
+const assert = require('assert');
+const hsl = require('./index.js');
+describe('hsl-to-hex', () => {
+  it('should convert values correctly', () => {
+    assert.deepStrictEqual(hsl(12, 1, 1), '#030303');
+  });
+  it('should convert values correctly', () => {
+    assert.deepStrictEqual(hsl(1014, 100, 100), '#ffffff');
+  });
+});
